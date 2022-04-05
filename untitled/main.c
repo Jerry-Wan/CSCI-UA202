1bf2ed16143ed360fa6f7cfd4840b9a24e97c9b9
Jerry-Wan <63923979+Jerry-Wan@users.noreply.github.com>
2022-04-04 20:59:40 -0400

    update

---
+++ untitled/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main() {
+    int algo;
+    int process;
+    char str[51];
+    int status;
+    int p;
+    char command;
+    printf("Enter the number of processes\n");
+    while(fgets( process, 50, stdin) <=0 && fgets( process, 50, stdin)!= NULL){
+        printf("Enter the number of processes\n");
+    }
+    printf("Enter the scheduling algorithm to use. 1 signifies FIFO; 2 signifies RR with quantum q\n");
+    while(fgets( algo, 50, stdin) != 1 && fgets( algo, 50, stdin) != 2 && fgets( algo, 50, stdin)!= NULL){
+        printf("Enter the scheduling algorithm to use. 1 signifies FIFO; 2 signifies RR with quantum q\n");
+    }
+
+        printf("%d %d",process,algo);
+    return 0;
+}
